2993102d4584e73735472d406184483ee9783ebe
Russell <russell_r_tsai@dimerco.com>
2018-01-09 10:50:39 +0800

    Initial Commit

---
+++ UnitTest_Demo3/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  UnitTest_Demo3
+//
+//  Created by APP Dimerco on 09/01/2018.
+//  Copyright © 2018 APP Dimerco. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
